d9d6c4d896aedfd4ce97e0571ff2df89e9158211
Mohamed <ibrahimthird7@gmail.com>
2026-01-23 21:10:29 +0530

    add script

---
+++ Logic/Form Validation/script.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+var userName = document.getElementById("userName");
+var email = document.getElementById("email");
+var btn = document.getElementById("btn");
+var form = document.querySelector("form");
+
+console.log(userName, email, btn, form);
